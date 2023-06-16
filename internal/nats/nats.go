915b89970b80c8a300a58bce003348640df857f1
DubinaAnt <Mixflash.rus@gmail.com>
2023-06-16 21:34:52 +0300

    update

--- internal/nats/nats.go
+++ internal/nats/nats.go
@@ -1,20 +1,61 @@
-package main
+package nats
 
 import (
-	"strconv"
-	"time"
+	"WBL0/internal/cache"
+
+	"WBL0/internal/model"
+	"WBL0/internal/repository"
+	"encoding/json"
+	"log"
 
 	"github.com/nats-io/stan.go"
+	"github.com/spf13/viper"
 )
 
-func main() {
+func NewConnection() (stan.Conn, error) {
+	natsConnection, err := stan.Connect(viper.GetString("nats.stanclusterid"), viper.GetString("nats.clientid"))
+	if err != nil {
+		return nil, err
+	}
+	return natsConnection, err
+}
 
-	sc, _ := stan.Connect("prod", "simple-pub")
-	defer sc.Close()
+func NewNatsSubscriber(conn stan.Conn, repos *repository.Repository, cache *cache.Cache) stan.Subscription {
 
-	for i := 1; ; i++ {
-		sc.Publish("bestellungen", []byte("Bestellung "+strconv.Itoa(i)))
-		time.Sleep(2 * time.Second)
+	handler := func(msg *stan.Msg) {
+		var order model.Order
+
+		err := json.Unmarshal(msg.Data, &order)
+		if err != nil {
+			log.Printf("unmarshal error: %s", err.Error())
+			err = msg.Ack()
+			if err != nil {
+				log.Printf("nats handler: %s", err.Error())
+			}
+			return
+		}
+
+		_, err = repos.CreateOrder(order)
+		if err != nil {
+			log.Printf("create order db error: %s", err.Error())
+			return
+		}
+		cache.Set(order.UID, order)
+		err = msg.Ack()
+		if err != nil {
+			log.Printf("nats handler ask: %s", err.Error())
+		}
 	}
 
+	sub, err := conn.Subscribe(
+		"order",
+		handler,
+		stan.DurableName("stand1"),
+		stan.SetManualAckMode(),
+	)
+	if err != nil {
+		log.Fatalf("create subscribe error: %s", err.Error())
+	}
+
+	return sub
 }
